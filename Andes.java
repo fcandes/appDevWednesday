2c5150809e390303ebe0a1379d48f7013013bc81
fcandes <fcandes@paterostechnologicalcollege.edu.ph>
2025-03-05 19:48:28 +0800

    Update Andes.java

--- Andes.java
+++ Andes.java
@@ -3,6 +3,6 @@ public class Andes {
         int age = 21; // Updated age
         System.out.println("Hello! This is Andes contributing to the project.");
         System.out.println("My current age is: " + age);
-        System.out.println("Next month, I will be: " + (age + 1));
+        System.out.println("Next year, I will be: " + (age + 1));
     }
 }
